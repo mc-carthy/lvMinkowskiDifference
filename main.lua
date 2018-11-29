cec15e6df682914ab9670792c4bd0667cc0726eb
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-11-29 21:20:09 +0000

    Resolve collisions

--- main.lua
+++ main.lua
@@ -38,13 +38,19 @@ function love.update(dt)
     keyboardBox.y = keyboardBox.y + dy
 
     calculateMinkowskiRect(mouseBox, keyboardBox)
+
+    if pointInRect({ x = 0, y = 0}, minkowskiBox) then
+        local dx, dy = calculateMinkowskiResolution()
+        keyboardBox.x = keyboardBox.x + dx
+        keyboardBox.y = keyboardBox.y + dy
+    end
 end
 
 function love.draw()
     if pointInRect({ x = 0, y = 0}, minkowskiBox) then
-        love.graphics.setColor(1, 1, 1, 1)
-    else
         love.graphics.setColor(1, 0, 0, 1)
+    else
+        love.graphics.setColor(1, 1, 1, 1)
     end
     love.graphics.rectangle('line', minkowskiBox.x, minkowskiBox.y, minkowskiBox.w, minkowskiBox.h)
     love.graphics.rectangle('line', mouseBox.x, mouseBox.y, mouseBox.size, mouseBox.size)
@@ -65,9 +71,44 @@ function calculateMinkowskiRect(boxA, boxB)
     minkowskiBox.h = boxA.h + boxB.h
 end
 
+function calculateMinkowskiResolution()
+    local x1, x2 = minkowskiBox.x, minkowskiBox.x + minkowskiBox.w
+    local y1, y2 = minkowskiBox.y, minkowskiBox.y + minkowskiBox.h
+
+    local minX = smallestAbs{x1, x2}
+    local minY = smallestAbs{y1, y2}
+
+    if math.abs(minX) < math.abs(minY) then
+        return minX, 0
+    else
+        return 0, minY
+    end
+end
+
 function pointInRect(point, rect)
     return (
         point.x > rect.x and point.x < rect.x + rect.w and
         point.y > rect.y and point.y < rect.y + rect.h
     )
+end
+
+function smallestAbs(values)
+    local squaredValues = {}
+    for i, v in ipairs(values) do
+        table.insert(squaredValues, math.pow(v, 2))
+    end
+
+    table.sort(squaredValues, function(a, b)
+        return a < b
+    end)
+
+    local smallestSquaredVal = squaredValues[1]
+
+    for _, v in pairs(values) do
+        if math.pow(v, 2) == smallestSquaredVal then
+            return v
+        end
+    end
+
+    return nil
 end
